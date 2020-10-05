2c0213f1bb733a89af5d022108e1b26fdc6c4953
Milind <m.milind15@gmail.com>
2020-10-05 15:10:06 +0530

    Changes Rollbacked

--- StressedByEmmEss/urls.py
+++ StressedByEmmEss/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = "Stresses By EmmEss Administartion"
 admin.site.site_title = "Stresses By EmmEss Admin Panel"
@@ -9,4 +11,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('menu/', include('menu.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- menu/models.py
+++ menu/models.py
@@ -4,19 +4,24 @@ class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     product_name = models.CharField(default="", max_length=300)
 
-    category = (
-        ("A","Brigadeiro"),
-        ("B","Brownie"),
-        ("C","Cake"),
-        ("D","Cheesecake"),
-        ("E","Chef's Special"),
-        ("F","Cupcakes"),
-        ("G","Fudges"),
-        ("H","Tea Time"),
+    categories = (
+        ("Brigadeiro","Brigadeiro"),
+        ("Brownie","Brownie"),
+        ("Cake","Cake"),
+        ("Cheesecake","Cheesecake"),
+        ("Chef's Special","Chef's Special"),
+        ("Cupcakes","Cupcakes"),
+        ("Fudges","Fudges"),
+        ("Tea Time","Tea Time"),
     )
 
-    product_category = models.CharField(max_length = 300, choices=category)
+    product_category = models.CharField(max_length = 110, choices=categories, default ="")
 
-    # product_preview_image = models.ImageField()
+    product_preview_image = models.ImageField(upload_to="menu/images", default="")
+
+    product_price = models.PositiveIntegerField(default = 0)
+
+    def __str__(self):
+        return self.product_name + "- Rs " + self.prduct_price
 
     
