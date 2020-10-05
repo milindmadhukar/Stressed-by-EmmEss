a555e1c843a3a1bf957cd6b141d94f1d2f0ffb71
Milind <m.milind15@gmail.com>
2020-10-05 21:36:32 +0530

    CDN Added

--- home/templatetags/customtags.py
+++ home/templatetags/customtags.py
@@ -5,4 +5,9 @@ register = template.Library()
 @register.filter()
 def accessItem(value,index):
     return value[index]
+
+@register.filter()
+def linkCorrector(value):
+    value = str(value)
+    return "https://github.com/milindmadhukar/Stressed-by-EmmEss/blob/master/media/home/images/" + value[12:]
  
